8c9c56046575539d1fbbe38c965019821d3f10cd
VincentG42 <goyetvincent@gmail.com>
2024-04-08 17:00:35 +0200

    suprpession d une erreur de syntaxe dans playercard.js

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -30,7 +30,7 @@ function PlayerCard({ player }) {
                         </div> :
                     
                         <p> Sick</p>
-                    }}
+                    }
                     
                 </div >
             </div >
