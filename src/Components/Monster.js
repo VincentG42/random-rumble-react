ac7f02ffccb6ee6b636dfe6cfd1a5c6735c8c5c9
VincentG42 <goyetvincent@gmail.com>
2024-04-11 09:52:16 +0200

    retouche monster card

--- src/Components/Monster.js
+++ src/Components/Monster.js
@@ -17,7 +17,7 @@ const Monster = () => {
       <div className='h-72 flex items-end justify-center px-8 md:px-0'>
 
         
-        <div className="md:w-1/2 flex flex-row flex-wrap  justify-center md:justify-end items-end relative p-4 bg-blue-700 backdrop-filter backdrop-blur-md bg-opacity-10 border border gray-200 rounded-lg">
+        <div className="md:w-1/2 flex flex-row flex-wrap  justify-center md:justify-end items-end relative p-4 bg-red-700  bg-opacity-20 border border gray-200 rounded-lg">
         <h5 className="text-xl font-bold bg-gradient-to-tr from-indigo-600 via-blue-300 to-red-200 text-transparent bg-clip-text mb-4 text-center w-full">Sephiroth</h5>
           <img src={monsterAvatar} alt='monster' className="md:absolute left-0 bottom-0 bottom md:z-10 w-auto" />
           <div className="w-3/4 h-8 flex justify-end items-end">
